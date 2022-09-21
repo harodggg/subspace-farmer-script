5982bb6b512b21abbf8322f75280d0e2308af7d1
harodggg <haroldtiansheng@gmail.com>
2022-09-22 02:33:59 +0800

    fix error

--- run-farmer.sh
+++ run-farmer.sh
@@ -80,7 +80,7 @@ install_docker_pre() {
 	sudo apt-get remove docker docker-engine docker.io containerd runc || true
 	sudo apt-get install ca-certificates curl gnupg lsb-release
 	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /etc/apt/keyrings/docker.gpg
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	set -eu
 
