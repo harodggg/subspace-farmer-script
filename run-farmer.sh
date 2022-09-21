91a08591165a30e7f1ee0ae4df24fe587791e1f7
harodggg <haroldtiansheng@gmail.com>
2022-09-22 03:11:48 +0800

    fix docker-compose

--- run-farmer.sh
+++ run-farmer.sh
@@ -76,10 +76,10 @@ check() {
 }
 
 install_docker_pre() {
-	set -e
-	sudo apt-get remove docker docker-engine docker.io containerd runc || true
-	sudo apt-get install ca-certificates curl gnupg lsb-release
-	sudo apt-get install pass gnupg2
+	set +e 
+	sudo apt-get -y remove docker docker-engine docker.io containerd runc || true
+	sudo apt-get -y install ca-certificates curl gnupg lsb-release
+	sudo apt-get -y install pass gnupg2
 	sudo mkdir -p /etc/apt/keyrings
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
