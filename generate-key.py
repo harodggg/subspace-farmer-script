7eb2591d5ff89ac8585f6cf93be864b71efa927d
harodggg <haroldtiansheng@gmail.com>
2022-09-27 01:29:51 +0800

    update

--- generate-key.py
+++ generate-key.py
@@ -14,7 +14,7 @@ all_address=[]
 
 def generate_file_name():
     tl=localtime()
-    return "(UTC+8)0"+str(tl.tm_mon)+"-"+str(tl.tm_mday)+'-'+str(tl.tm_hour)+':'+str(tl.tm_min)+":"+str(tl.tm_sec)+".json"
+    return "(UTC+8)0"+str(tl.tm_mon)+"-"+str(tl.tm_mday)+'-'+str(tl.tm_hour)+':'+str(tl.tm_min)+":"+str(tl.tm_sec)
 
 
 def generate_address(ss58_format):
@@ -37,7 +37,7 @@ def main():
                 )
             print("{},".format(keypair[1])) 
         print("]")
-        with open(generate_file_name(), 'w') as f:
+        with open(generate_file_name() + ".json", 'w') as f:
             dump(all_address,f,sort_keys=True, indent=4, separators=(',', ':'))
     else: 
         print("Usage: {} options (num)".format(basename(argv[0])))
