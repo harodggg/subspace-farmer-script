cf11aa3317b78e2135370046270c921e74f72186
harodggg <haroldtiansheng@gmail.com>
2022-10-05 05:04:43 +0800

    fix bugs

--- read_address.sh
+++ read_address.sh
@@ -1,12 +1,17 @@
 work_path=$(pwd)
-node_num=1
-echo $work_path
+node_num=100      #  修改节点数
+node_name=sfarmer # 修改自己的节点名字
 for ((i = 1; i <= ${node_num}; i++)); do
 
-    config_path=$(pwd)/"sfarmer"$i
-    # cd $config_path
+    config_path=$work_path/"$node_name"$i
+    cd $config_path
     address=$(yq '.services.farmer.command[-3]' <docker-compose.yaml)
-    address=$address","
+    if [ $i -eq $node_num ]; then
+        address='"'$address'"'
+    else
+        address='"'$address'"'","
+    fi
+
     echo $address
 
 done
