ce4e040be8014ad345034638fa2f0ee651af6cbe
harodggg <haroldtiansheng@gmail.com>
2022-10-12 09:03:31 +0800

    add balance-json image

--- machines-farmer.sh
+++ machines-farmer.sh
@@ -1,29 +1,33 @@
 num_per_farmer=5
+
 node_name="anode"
 end_machine=12
+
 start_machine=1
 work_path=$(pwd)
 
 upgrade_node() {
-    export NAME=$1
-    yq -i '.services.farmer.deploy.placement.constraints[0]="node.labels.name == env(NAME)"' ./swarm-farmer.yaml
+    export NAME="node.labels.name =="$1
+    yq -i '.services.farmer.deploy.placement.constraints[0]=env(NAME)' ./swarm-farmer.yaml
     unset NAME
 
 }
 
-upgrade_per_famers() {
-    jq --arg $1 $num ".node_num=$num" ./swarm.json | sponge swarm.json
+upgrade_per_farmers() {
+    jq --arg num $1 ".node_num=$num" ./swarm.json >tmp.$$.json && mv ./tmp.$$.json ./swarm.json
 }
 
 run() {
     # upgrade_per_famers $num_per_farmer
     for ((i = $start_machine; i <= $end_machine; i++)); do
-        upgrade_node $((num_per_farmer * i))
+        num=$((num_per_farmer * i))
+        upgrade_per_farmers $num
         echo $((num_per_farmer * i))
         name=$node_name$i
         echo $name
         upgrade_node $name
-        $(./run-farmer.sh create only-farmer)
+        $work_path/run-farmer.sh swarm create
+        sleep 2
     done
 }
 
